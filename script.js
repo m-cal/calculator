67ad41a33cc04ff929cedb4cd6cda3c506e338f1
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-24 00:36:07 -0400

    Make basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello console');
+function add (a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+// Takes operator and 2 nums and calls one of the above functions on the numbers
+function operate () {
+  
+}
