9f59fae19fd49f9cf27e62ddeb2d8ee8a2cbcfc8
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-26 17:24:12 -0400

    Make partial logic; larger font

--- script.js
+++ script.js
@@ -34,19 +34,81 @@ function operate(operator, a, b) {
   }
 }
 
-let currentlyDisplayedNumber = document.querySelector('#currently-displayed-number');
+let currentlyDisplayedText = document.querySelector('#currently-displayed-text');
+let currentNumber = Number(currentlyDisplayedText.textContent);
+
+let currentOperator = null;
+
+let leftNum = null;
+
+function getCurrentNumber() {
+  currentNumber = Number(currentlyDisplayedText.textContent);
+  return currentNumber;
+}
 
 let numberButtons = document.querySelectorAll('.number-button');
+let operatorButtons = document.querySelectorAll('.operator-button');
 
 function loadEventListeners() {
   numberButtonEventListeners = () => {
     numberButtons.forEach(button => {
       button.addEventListener('click', (event) => {
-        currentlyDisplayedNumber.textContent += button.textContent.trim();
+        currentlyDisplayedText.textContent += button.textContent.trim();
       });
     });
   }
-  return numberButtonEventListeners();
+
+  operatorButtonEventListeners = () => {
+    operatorButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        if (currentOperator == null) {
+          leftNum = getCurrentNumber();
+          currentOperator = button.textContent.trim();
+          currentlyDisplayedText.textContent = '';
+        } else if (currentOperator) {
+          if ()
+
+        }
+      });
+    })
+  }
+
+  numberButtonEventListeners();
+  operatorButtonEventListeners();
 }
 
 loadEventListeners();
+
+
+
+/* 
+
+- number is entered with number buttons
+- have event listener for when operator is clicked
+   - when an op is clicked
+    - store number in a var 'leftNum'
+    - store op in a var 'currentOperator'
+    - clear #currently-displayed-text
+   - when another number is clicked:
+    - buttons should upate div
+   - wait until another operator is clicked
+    - if operator is = 
+      - call getCurrentNumber()
+      - update #currently displayed text to (operate(operator, leftNum, currentNumber));
+      - call getCurrentNumber
+    - if operator is 
+
+*/
+
+
+/* 
+
+- numbers and operators are stored as string value
+- check for when pattern changes from NumOpNum to NumOpNumOp
+  - if second op is =
+    - do final calc 
+  - if is + or - or * or /
+    - first calc NumOpNum and store that result
+    - then repeat with that result OpNum
+
+*/
